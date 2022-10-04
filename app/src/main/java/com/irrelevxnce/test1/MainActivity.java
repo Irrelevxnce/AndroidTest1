77b35fcfaacf1464d7be38abeecaa04e7528156a
Cameron Wilson <camw641@gmail.com>
2022-10-04 19:26:32 +0200

    I don't know what i am doing.

--- app/src/main/java/com/irrelevxnce/test1/MainActivity.java
+++ app/src/main/java/com/irrelevxnce/test1/MainActivity.java
@@ -3,12 +3,21 @@ package com.irrelevxnce.test1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button test1 = findViewById(R.id.button);
+        test1.setOnClickListener(this :: onClick);
+    }
+
+    public void onClick(View v) {
+
     }
 }
